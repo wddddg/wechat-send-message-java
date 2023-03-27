7755bc1e97d430ac908ea949164e5293a91d2564
chendr <cdrcjxcly@qq.com>
2023-03-27 09:30:49 +0800

    test

---
+++ src/main/java/cn/wddxhz/WechatSendMessageApplication.java
@@ -0,0 +1,13 @@
+package cn.wddxhz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatSendMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatSendMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/wddxhz/WechatSendMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wddxhz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatSendMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
